7e67d75b386c5caff0a81dc1a2cb9180e0174fbf
smickel20 <75023071+smickel20@users.noreply.github.com>
2022-03-09 15:53:22 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,21 @@ function zoomin() {
             var currWidth = GFG.clientWidth;
             GFG.style.width = (currWidth - 100) + "px";
         }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
