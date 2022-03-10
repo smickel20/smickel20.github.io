de28076b8f99dad4711be5ebc2dae70a2b1a4423
mick <mick@purpleone.nl>
2022-03-10 15:50:05 +0100

    scroll img

--- js/script.js
+++ js/script.js
@@ -1,29 +1,6 @@
-function zoomin() {
-            var GFG = document.getElementById("geeks");
-            var currWidth = GFG.clientWidth;
-            GFG.style.width = (currWidth + 100) + "px";
-        }
-          
-        function zoomout() {
-            var GFG = document.getElementById("geeks");
-            var currWidth = GFG.clientWidth;
-            GFG.style.width = (currWidth - 100) + "px";
-        }
+let scroll = document.getElementById("scroll")
+scroll.onscroll = imgResize
 
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
+function imgResize(){
+    document.querySelector("scroll").style.transform = "scale(1.0)"
 }
