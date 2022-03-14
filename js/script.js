73032f1a8afc24017603d41f79e8d294972e0c8c
mick <mick@purpleone.nl>
2022-03-14 09:00:49 +0100

    opnieuwe beginnen

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("scroll").style.transform = "scale(1.0)";
-    }
-}
