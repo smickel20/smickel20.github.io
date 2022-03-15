b341a2cb5c92f6d82f5140de01a8b41b59cc714e
mick <mick@purpleone.nl>
2022-03-15 11:12:54 +0100

    scroll image

--- js/script.js
+++ js/script.js
@@ -1,28 +1,7 @@
-// (function() {
-//     (function($) {
-//         var $animateIn, $animateOut, animateInOffset, animateOutOffset;
-//         $animateIn = $('.animate-in');
-//         $animateOut = $('.animate-out');
-//         animateInOffset = 100;
-//         animateOutOffset = 200;
-//         $animateIn.addClass('pre-animate');
-//         $animateOut.addClass('pre-animate-out');
-//         return $(window).scroll(function(e) {
-//             var bottomScrollPosition, windowHeight, windowScrollPosition;
-//             windowHeight = $(window).height();
-//             windowScrollPosition = $(window).scrollTop();
-//             bottomScrollPosition = windowHeight + windowScrollPosition;
-//             $animateIn.each(function(i, element) {
-//                 if ($(element).offset().top + animateInOffset < bottomScrollPosition) {
-//                     return $(element).removeClass('pre-animate');
-//                 }
-//             });
-//             return $animateOut.each(function(i, element) {
-//                 if ($(element).offset().top + animateOutOffset < bottomScrollPosition) {
-//                     return $(element).removeClass('pre-animate-out');
-//                 }
-//             });
-//         });
-//     })(jQuery);
-//
-// }).call(this);
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("scroll").className = "animated";
+    }
+}
