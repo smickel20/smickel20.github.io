75108f8df377afff3bf8cee7fe848ed8a7487072
mick <mick@purpleone.nl>
2022-03-28 16:29:24 +0200

    documentatie js en de animatie beter

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
 window.addEventListener("load",   function (){
     var pics = document.getElementById("imgs");
-    var height
-
-    window.addEventListener("resize", adjustHeightVars);
+    // maakt een variable van imgs
+    document.getElementById("myimg").style.visibility = "hidden";
+    //laat de img niet zien als je er nog niet voorbij bent gescrolt
     window.addEventListener("scroll", fadeBox);
+    //kijkt of je scrollt
+    var startAnimation = pics.offsetTop - window.innerHeight
+    // hier kijkt het waar het plaatje is en dan doet het min waar je zit op de page en dan weet je ongeveer waar de top van het plaatjes is
 
     function fadeBox () {
-        var y = pics.offsetTop - 1100;
-        var x = window.pageYOffset;
-        if (x >= y) {
+        var PicHeight = pics.offsetTop - startAnimation;
+        // bekijkt de hoogte van de img
+        var PageHeight = window.pageYOffset;
+        // bekijkt de hoogte van de pagina
+        if (PageHeight >= PicHeight) {
+            document.getElementById("myimg").style.visibility = "visible";
+            //laat de img zien
             pics.classList.add("fadein")
+            // speelt de animatie af
         }
     }
 
-    function adjustHeightVars() {
-        height = window.innerHeight;
-    }
-
-    adjustHeightVars();
 })
